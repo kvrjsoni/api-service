e95c8889ecfd4130e8c409e029f50a1166972c3a
Kaviraj <kvrjsoni@gmail.com>
2022-06-27 22:53:30 +0530

    INITIALISED api-service repo, ADDED gin for APIs, ADDED default route

---
+++ helpers/token.go
@@ -0,0 +1,7 @@
+package helpers
+
+import "github.com/gin-gonic/gin"
+
+func GenerateToken(c *gin.Context) {
+
+}
---
+++ routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kvrjsoni/api-service/helpers"
+)
+
+func initializeRoutes() {
+	router := gin.Default()
+	router.GET("/", defaultRoute)
+	router.GET("/token/generate", helpers.GenerateToken)
+	router.Run(":3001")
+}
+
+func defaultRoute(c *gin.Context) {
+	c.String(http.StatusOK, "Welcome to API service")
+}
