f468a97d5b8d0c07237d53284690024c2715ea8e
Kaviraj <kvrjsoni@gmail.com>
2022-06-27 23:55:01 +0530

    ADDED gorm for DB operations, ADDED DB connection at server start

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/kvrjsoni/api-service/models"
+
 func main() {
+	models.ConnectDatabase()
 	initializeRoutes()
 }
---
+++ models/database.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "kvrjsoni:nua@123@tcp(127.0.0.1:3306)/api_service?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	DB = db
+}
